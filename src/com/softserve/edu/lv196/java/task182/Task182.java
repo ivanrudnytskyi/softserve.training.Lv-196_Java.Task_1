5d0045dd3e1c0cfa6aedd457e6877487c493c982
ivanrudnytskyi <ivan.rudn@gmail.com>
2016-08-31 11:19:30 +0300

    added src folder

---
+++ src/com/softserve/edu/lv196/java/task182/Task182.java
@@ -0,0 +1,63 @@
+package com.softserve.edu.lv196.java.task182;
+
+import java.util.LongSummaryStatistics;
+import java.util.stream.LongStream;
+
+/**
+ * @author Ivan Rudnytskyi
+ * date 2016/08/31 (yyyy/mm/dd)
+ *
+ * The task (# 182):
+ *
+ * A natural number is given. Also a sequence of the whole numbers is given - a1, ... an.
+ * Find count and sum of the members of the sequence, which divide by 5 and don't divide by 7.
+ *
+ *
+ */
+
+public class Task182 {
+
+    public static void main(String[] args) {
+
+        //the high limit for the sequence
+        long n = 39;
+
+        //the start number of the sequence
+        long a1 = -26;
+
+        //the method, which searches for numbers, which a divided by 5 and not divided
+        // by 7 - findDivisorsBy5andNotBy7
+        findDivisorsBy5andNotBy7(a1, n);
+    }
+
+    /**
+     * @param a1 - long, the start number of the sequence
+     * @param n  - long, the high limit for the sequence
+     *           Numbers, which divide by 5 and don't divide by 7, are printed. If no such numbers
+     *           are found - the message is printed.
+     * @throws IllegalArgumentException - the numbers must be greater or equal to 0 and
+     *                                  high limit must be greater than the start number
+     */
+    public static void findDivisorsBy5andNotBy7(long a1, long n) {
+
+        if (n < 0 || n < a1)
+            throw new IllegalArgumentException("check input data");
+
+        LongSummaryStatistics stat = LongStream.rangeClosed(a1, n)
+                .filter(i -> (i % 5 == 0 && i % 7 != 0))
+                .summaryStatistics();
+
+        //if the numbers are found
+        if (stat.getCount() > 0) {
+            System.out.println("Count of numbers, which divides by 5 and not by 7 "
+                    + "under number " + n + ": " + stat.getCount());
+            System.out.println("Sum of numbers, which divides by 5 and not by 7 "
+                    + "under number " + n + ": " + stat.getSum());
+        } else {
+            //otherwise - print the message
+            System.out.println("There are no numbers, which divide by 5 and not by 7" +
+                    " under number " + n);
+        }
+
+    }
+}
---
+++ src/com/softserve/edu/lv196/java/task330/Task330.java
@@ -0,0 +1,56 @@
+package com.softserve.edu.lv196.java.task330;
+
+import java.util.stream.LongStream;
+
+/**
+ * @author Ivan Rudnytskyi
+ * date 2016/08/31 (yyyy/mm/dd)
+ *
+ * The task (#330):
+ *
+ * A natural number is perfect, if it equals to sum of its divisors, except itself.
+ * Number 6 is perfect, because 6 = 1 + 2 + 3. Number 8 is not perfect, since 8 != 1 + 2 + 4.
+ * A natural number n is given. Find all the perfect numbers under the number n.
+
+ *
+ **/
+
+
+public class Task330 {
+
+    public static void main(String[] args) {
+
+        //the limit for perfect numbers search
+        long number = 10_000;
+
+        // the method, which searches for perfect numbers
+        perfectNumbers(number);
+
+    }
+    /**
+     * in the method perfect number are searched for. If a perfect number is found
+     *  - it is printed. Otherwise, the message is printed - no perfect numbers found.
+     * @param number - long, high limit of the search for perfect numbers
+     * @throws IllegalArgumentException - the number must be greater than 1
+     */
+    public static void perfectNumbers (long number){
+
+        if (number < 1)
+            throw new IllegalArgumentException("The number must be greater than 1");
+
+        //prints and count perfect numbers
+        long count = LongStream.rangeClosed(1, number)
+                .filter(n -> n == LongStream.rangeClosed(1, n / 2)
+                        .filter(i -> n % i == 0)
+                        .sum())
+                .peek(i -> System.out.println("Perfect number under " + number + ": " + i))
+                .count();
+
+        //otherwise - if no perfect number is found, print the message
+        if (count == 0)
+            System.out.println("There are no perfect numbers under " + number);
+
+        System.out.println("Calculation finished");
+    }
+
+}
